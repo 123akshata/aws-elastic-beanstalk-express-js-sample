156f2e6f9d5ab6c80c96a99d63b6c23c2973a711
Akshata Hirekodi (MINDTREE LIMITED) <v-akhire@microsoft.com>
2021-10-19 12:58:53 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Craze for AWS'));
+app.get('/', (req, res) => res.send('Almost completed CI & CD concept'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
